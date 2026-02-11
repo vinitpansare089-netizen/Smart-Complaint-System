d9a7ed20e1a7763915aae196a2590286eac55bc6
vinitpansare089-netizen <vinitpansare089@gmail.com>
2026-02-11 20:02:31 +0530

    changes

--- backend/db.py
+++ backend/db.py
@@ -1,7 +1,36 @@
+# import os
+# from pymongo import MongoClient
+
+# MONGO_URL = os.getenv("MONGO_URL")
+
+# client = MongoClient(MONGO_URL)
+
+# db = client["complaint_admin"]
+
+# complaints_collection = db["complaints"]
+# Responsible_faculty = db["responsible_faculty"]
+# print("DEBUG MONGO_URL =", MONGO_URL)
+
 import os
 from pymongo import MongoClient
+from dotenv import load_dotenv
 
+# Get path of backend folder
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# Point directly to backend/.env
+ENV_PATH = os.path.join(BASE_DIR, ".env")
+
+# Load it
+load_dotenv(ENV_PATH)
+
+# Read variable
 MONGO_URL = os.getenv("MONGO_URL")
+
+print("DEBUG MONGO_URL =", MONGO_URL)
+
+if not MONGO_URL:
+    raise ValueError("❌ MONGO_URL is missing")
 
 client = MongoClient(MONGO_URL)
 
@@ -9,4 +38,3 @@ db = client["complaint_admin"]
 
 complaints_collection = db["complaints"]
 Responsible_faculty = db["responsible_faculty"]
-print("DEBUG MONGO_URL =", MONGO_URL)
